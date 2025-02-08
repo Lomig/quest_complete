e2e0e17f84105cc2804e4fd0d8ed7d8dc0f6e2ca
Lomig Enfroy <lomig@hey.com>
2025-02-08 22:18:41 +0100

    Add Games Domain object and Game Show

--- lib/server/router.ml
+++ lib/server/router.ml
@@ -1,5 +1,8 @@
 open Handlers
 
 let routes =
-  [ Dream.get "/static/**" (Dream.static "static"); Dream.get "/" Pages.homepage ]
+  [ Dream.get "/static/**" (Dream.static "static")
+  ; Dream.get "/" Pages.homepage
+  ; Dream.get "/games/:id" Games.show
+  ]
 ;;
