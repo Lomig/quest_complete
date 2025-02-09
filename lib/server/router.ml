c5e19e75dca87d0f3bdce43e7c81934c2d5008c7
Lomig Enfroy <lomig@hey.com>
2025-02-09 16:52:47 +0100

    Add Games Index

--- lib/server/router.ml
+++ lib/server/router.ml
@@ -3,6 +3,7 @@ open Handlers
 let routes =
   [ Dream.get "/static/**" (Dream.static "static")
   ; Dream.get "/" Pages.homepage
+  ; Dream.get "/games" Games.index
   ; Dream.get "/games/:id" Games.show
   ]
 ;;
--- lib/server/views/games.ml
+++ lib/server/views/games.ml
@@ -1,6 +1,24 @@
 open Containers
 open Domain
 
+let index_data (games : Game.t list) : Templates.Games.Index.data =
+  List.map
+    (fun (game : Game.t) : Templates.Games.Partials.IndexGame.data ->
+       { uri = "/games/" ^ game.id
+       ; title = game.title
+       ; rating =
+           (match game.rating with
+            | None -> "N/A"
+            | Some r -> string_of_int r)
+       ; cover_url = game.cover_url
+       })
+    games
+;;
+
+let index games =
+  Templates.Layouts.Main.layout @@ Templates.Games.Index.render @@ index_data games
+;;
+
 let show_data (game : Game.t) : Templates.Games.Show.data =
   { title = game.title
   ; release_date = Date.to_short game.release_date
